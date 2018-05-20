22a27d2ccaa99eb64af0d0328a9d119dacdcbf91
Incinirate <dogescript@gmail.com>
2018-05-19 23:26:09 -0600

    fix fatal crash

--- src/sections/updates.lua
+++ src/sections/updates.lua
@@ -12,13 +12,10 @@ if config.checkForUpdates ~= false then
           logger.warn("Version mismatch, latest release is "
             .. release.tag_name .. ", but running version is " .. versionTag)
         end
-
-        jua.off("http_failure", fail)
       end 
     else
       if url == ghURL then
         logger.warn("Unable to fetch release data")
-        jua.off("http_success", succ)
       end
     end
   end, ghURL)
