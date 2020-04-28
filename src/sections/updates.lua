e9346deaf81a9544be04ccc1d3cc6df8c9029212
Incinirate <dogescript@gmail.com>
2020-04-28 01:49:12 -0600

    Update versioning code

--- src/sections/updates.lua
+++ src/sections/updates.lua
@@ -1,26 +1,25 @@
-local ghURL = "https://api.github.com/repos/incinirate/Xenon/releases/latest"
+local versionURL = "http://xenon.its-em.ma/version"
 
 if config.checkForUpdates ~= false then
   rapi.request(function(success, url, handle)
     if success then
-      if url == ghURL then
-        local releaseData = handle.readAll()
+      if url == versionURL then
+        local release = handle.readAll()
         handle.close()
 
-        local release = json.decode(releaseData)
-        if release.tag_name ~= versionTag then
+        if release ~= versionTag then
           logger.warn("Version mismatch, latest release is "
-            .. release.tag_name .. ", but running version is " .. versionTag)
+            .. release .. ", but running version is " .. versionTag)
 
-          if release.tag_name:match("v(%d+)") ~= versionTag:match("v(%d+)") then
+          if release:match("v(%d+)") ~= versionTag:match("v(%d+)") then
             logger.warn("Latest version has a major version seperation gap, it may not be safe to update. Review the changelog for more details.")
           end
         end
-      end 
+      end
     else
-      if url == ghURL then
+      if url == versionURL then
         logger.warn("Unable to fetch release data")
       end
     end
-  end, ghURL)
+  end, versionURL)
 end
